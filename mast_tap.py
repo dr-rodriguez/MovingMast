3b6f04a0133df40e8c7ae235e7854e3bdf505b9e
David Rodriguez <drodriguez@stsci.edu>
2020-09-01 13:56:55 -0400

    adding some quick coding for the tap query

--- mast_tap.py
+++ mast_tap.py
@@ -5,12 +5,23 @@ import warnings
 warnings.simplefilter('ignore')  # block out warnings
 
 
+def convert_stcs_for_adql(stcs):
+    adql = "POLYGON('ICRS', "
+    for elem in stcs.split():
+        if elem == 'POLYGON': continue
+        if elem == '': continue
+        adql += f'{elem}, '
+    adql = adql[:-2] + ')'
+    return adql
+
+
 def run_query(stcs, start_time, end_time, mission=None,
-              service='http://vao.stsci.edu/CAOMTAP/TapService.aspx', maxrec=1000):
+              service='http://vao.stsci.edu/CAOMTAP/TapService.aspx', maxrec=100):
     tap = vo.dal.TAPService(service)
 
-    # TODO: write this
-    query = "SELECT * FROM dbo.ObsPointing"
+    # TODO: add start/end time
+    query = f"SELECT TOP {maxrec} * FROM dbo.ObsPointing " \
+            f"WHERE 1=CONTAINS(POINT('ICRS', s_ra, s_dec), {convert_stcs_for_adql(stcs)})"
     print(query)
 
     # TODO: Decide: Sync vs Async queries
@@ -23,4 +34,3 @@ def run_query(stcs, start_time, end_time, mission=None,
     # results = job.fetch_result()
 
     return results.to_table()
-
