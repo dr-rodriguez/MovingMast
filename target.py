5201ca9e8e15a74041f7c938cacc565ec1c89635
David Rodriguez <drodriguez@stsci.edu>
2020-09-01 12:48:02 -0400

    some more additions to target functions

--- target.py
+++ target.py
@@ -3,9 +3,14 @@
 from astroquery.jplhorizons import Horizons
 
 
-def get_path(obj_name, times, location='@TESS'):
+def get_path(obj_name, times, location=None):
+    # See more details at https://astroquery.readthedocs.io/en/latest/jplhorizons/jplhorizons.html
+    # For valid locations: https://minorplanetcenter.net//iau/lists/ObsCodesF.html
     obj = Horizons(id=obj_name, location=location, epochs=times)
     eph = obj.ephemerides()
-    ra = eph['RA'][0]
-    dec = eph['DEC'][0]
-    return ra, dec
+    return eph
+
+
+def convert_path_to_polygon(eph):
+    # TODO: Implement this
+    return
