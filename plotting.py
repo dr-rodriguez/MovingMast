d3424603eaa9368df7cb776f34d1436a2428f3e6
Clara Brasseur <cbrasseur@stsci.edu>
2020-09-01 17:03:01 -0400

    adding a notebook

--- plotting.py
+++ plotting.py
@@ -1,9 +1,27 @@
 # Functions to handle plotting
 
-from bokeh.plotting import figure, output_file, show
+from bokeh.plotting import figure, output_file, show, output_notebook
 from polygon import parse_s_region
+from bokeh.models import Arrow, VeeHead
 import matplotlib.pyplot as plt
 
+
+def notebook_bokeh(stcs):
+    patch_xs = parse_s_region(stcs)['ra']
+    patch_ys = parse_s_region(stcs)['dec']
+
+    p = figure(plot_width=700, x_axis_label="RA (deg)", y_axis_label="Dec (deg)")
+
+    data = {'x': [patch_xs], 'y': [patch_ys]}
+    p.patches('x', 'y', source=data, fill_alpha=0.1, line_color="black", line_width=0.5)
+
+    p.add_layout(Arrow(end=VeeHead(line_color="black", line_width=1), line_width=2,
+                       x_start=patch_xs[0], y_start=patch_ys[0], 
+                       x_end=patch_xs[1], y_end=patch_ys[1]))
+
+    output_notebook()
+    show(p)
+    
 
 def quick_bokeh(stcs, outfile='test.html'):
     patch_xs = parse_s_region(stcs)['ra']
